5c1d8696e62007779a3cdd06c8e03744e1d98b8e
Haska Fikri Tauhid <haskafikritauhid@gmail.com>
2020-03-15 19:42:19 +0700

    add options

--- fe/libs/mds-api/Core.ts
+++ fe/libs/mds-api/Core.ts
@@ -6,7 +6,9 @@ import { isStringAndValid } from "mds/fe/libs/utils";
 export interface IConfig {}
 export interface IMdsConfig {}
 export interface ICookie {}
-export interface ICoreOptions {}
+export interface ICoreOptions {
+  defaultTimeZone?: string;
+}
 
 class MdsConfig {
   constructor(config: IConfig) {}
@@ -17,10 +19,12 @@ class MdsCookie extends Cookie {}
 class MdsCore {
   config: MdsConfig;
   cookie: MdsCookie;
+  options: ICoreOptions;
 
   constructor(config: IConfig, cookie: ICookie, options: ICoreOptions) {
     this.config = new MdsConfig(config);
     this.cookie = new MdsCookie(cookie);
+    this.options = options;
   }
 
   isPersist(expiration: string) {
@@ -36,7 +40,7 @@ class MdsCore {
     return new Buffer(now).toString("base64");
   }
 
-  handleSession() {
+  handleSession(): string {
     const cookie: ICookie = this.cookie.getAll();
 
     const conditionOk = where({
